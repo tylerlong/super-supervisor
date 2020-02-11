1ba58b60b3f66981e99d48b3a7c112dc2e3a47dd
Tyler Long <tyler4long@gmail.com>
2020-02-10 16:31:25 -0800

    Add ClientInfo

---
+++ src/ClientInfo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class ClientInfo extends React.Component {
+  render () {
+    return <div>Client Info</div>
+  }
+}
+
+export default ClientInfo
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// import ClientInfo from './ClientInfo'
+import ClientInfo from './ClientInfo'
 
 const container = document.createElement('div')
 document.body.appendChild(container)
-// const clientInfo = new ClientInfo()
-// ReactDOM.render(clientInfo, container)
-
-ReactDOM.render(<h1>test</h1>, container)
+ReactDOM.render(<ClientInfo />, container)
